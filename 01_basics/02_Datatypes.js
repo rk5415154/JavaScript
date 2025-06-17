22379397e8a11d20b32a30466fc90fd589695ddc
Raj Kumar <154978629+rk5415154@users.noreply.github.com>
2025-06-17 17:40:23 +0000

    DataType done

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all js code as newer version
+//alert(3+3)//we are using node.js not browser
+
+console.log(3+3)
+//code readsbility should high
+let name ="Raj Kumar"
+let age=19
+let isloggedIN= true
+
+//number => 2 to power of 53
+//bigint
+
+//string =>""
+//boolean => true or false
+//null => standalone value
+// undefinded => do not assign the value now
+//symbol => unique
+//object
+// null also a object
+console.log(typeof "Raj Kumar");
+console.log(typeof undefined) // o/p = undefined
+console.log(typeof null)//o/p = object
